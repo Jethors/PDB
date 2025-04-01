e40eebb1583041ccf68091a4c55f13d4227d338d
Jethors <dens@hotmail.se>
2025-04-02 00:06:19 +0200

    Update SAP_From_List.py

--- SAP_From_List.py
+++ SAP_From_List.py
@@ -1,12 +1,12 @@
 import SAP_XML as sap
 import os
 
-def SAP_From_List(PDB_List, PDB_files_map, directory=f"{os.getcwd()}/files"):
+def SAP_From_List(PDB_List, PDB_files_folder, directory=f"{os.getcwd()}/files"):
     """Run SAP-score for a list of PDB-Ids
     Arguments
     PDB_ID: PDB ID of the protein
-    PDB_files_map: name of the map which contain the proteins PDB file
-    directory: path to the map where the PDB_files_map is located. (defaults to working directory for the program /files)
+    PDB_files_folder: name of the folder which contain the proteins PDB file
+    directory: path to the folder where the PDB_files_folder is located. (defaults to working directory for the program /files)
     """
     for PDB_ID in PDB_List:
-        sap.SAP_XML(PDB_ID, PDB_files_map, directory)
+        sap.SAP_XML(PDB_ID, PDB_files_folder, directory)

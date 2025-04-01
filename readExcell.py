850c771fe8d4084ffa1f780287b246ee075ecb40
Jethors <dens@hotmail.se>
2025-04-01 16:04:27 +0200

    Update readExcell.py

--- readExcell.py
+++ readExcell.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 def read_excel_column(file_path, column_index=1):
-    """Reads an Excel file and returns a list of values from the specified column index."""
+    """Reads an Excel file and returns a list of values from the specified column index defaults to second column.
+    Arguments
+    file_path: path to the excell file
+    column_index: Index of the column in the excell file which should become a list
+
+    Can give full filepath to the excell document or /files/excelllist.xlsx
+    """
     try:
-        df = pd.read_excel(file_path)
-        return df.iloc[:, column_index].tolist()
+        df = pd.read_excel(file_path)  # read document
+        return df.iloc[:, column_index].tolist() # return list of selected column
     except Exception as e:
         print(f"Error reading {file_path}: {e}")
         return []

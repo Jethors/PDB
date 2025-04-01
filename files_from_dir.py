bd9fad47a52fee6edbe304319813a36884b632e0
Jethors <dens@hotmail.se>
2025-04-01 16:02:59 +0200

    Update files_from_dir.py

--- files_from_dir.py
+++ files_from_dir.py
@@ -1,9 +1,14 @@
 import os
 
-def files_from_dir(map, directory="files"):
+def files_from_dir(map, directory=f"{os.getcwd()}/files"):
+    """Finds all .pdb files in a map and returns a list of the PDB IDs
+    Arguments:
+    map: The map containing PDB files
+    directory: path to the map where the selected map is located. (defaults to files in working directory)
+    """
     list2=[]
-    list= os.listdir(f"{os.getcwd()}/{directory}/{map}")
+    list= os.listdir(f"{directory}/{map}") #lists everything in the map
     for file in list:
-        if file.endswith(".pdb"):
-            list2.append(file[:-4])
+        if file.endswith(".pdb"):  #check if its a pdb file
+            list2.append(file[:-4])  #remove .pdb
     return list2
